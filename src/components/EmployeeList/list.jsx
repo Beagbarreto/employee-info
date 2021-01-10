70f218afb02cdb19a5ac1f75550467a81da58fb6
Beagbarreto <beagbarreto09@gmail.com>
2021-01-10 17:22:23 -0600

    feat: hardcoded list into List component

--- src/components/EmployeeList/list.jsx
+++ src/components/EmployeeList/list.jsx
@@ -1,13 +1,26 @@
 import React, { useState } from 'react';
-
+ 
 function List() {
-const [employeeList, setEmployeeList] = useState('');
-
-  return (
-    <div>
-      <h1>List belongs here</h1>
-      </div>
-  )
+const [employees, setEmployees] = useState([
+{name: 'Roger Vormittog', address: '11 Jackson Av, Scarsdale, NY', id: 1},
+{name: 'Giselle knowles', address: '15 Heathcote Road, Scarsdale, NY', id: 2},
+{name: 'Lauren George', address: '53 Winter Hill Rd, Tuckahoe, NY', id: 3}
+]);
+ 
+ return (
+   <div>
+     <h1>List belongs here</h1>
+     <div>
+     {employees.map((employee) => (
+       <div key={employee.id}>
+         <ul>
+           <li>{employee.name}</li>
+         </ul>
+       </div>
+     ))}
+     </div>
+     </div>
+ )
 }
-
+ 
 export default List;
