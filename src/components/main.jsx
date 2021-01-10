99b9168fca699bda77940436686254f8ade49c50
Beagbarreto <beagbarreto09@gmail.com>
2021-01-10 15:03:27 -0600

    feat: implemented router

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -11,19 +11,20 @@ import List from './EmployeeList/list';
 const Main = () => {
 
     return (
-      <HashRouter>
-        <div>
-          <h1>Employee App</h1>
-          <ul className="header">
-            <li><a href="/">Home</a></li>
-            <li><a href="/stuff">Form</a></li>
-            <li><a href="/contact">List</a></li>
-          </ul>
-          <div className="content">
-             
-          </div>
-        </div>
-        </HashRouter>
+<HashRouter>
+<div>
+  <h1>Employee App</h1>
+  <ul className="header">
+    <li><NavLink to="/">Form</NavLink></li>
+    <li><NavLink to="/list">List</NavLink></li>
+  </ul>
+  <div className="content">
+    <Route exact path="/" component={Form}/>
+    <Route path="/list" component={List}/>
+  </div>
+</div>
+</HashRouter>
+
     );
 }
  
