a5b1c259c1d851f500610149b1d6b0622f545b5d
Beagbarreto <beagbarreto09@gmail.com>
2021-01-11 22:48:45 -0600

    feat: added all missing input  fields to form component

--- src/components/EmployeeForm/form.jsx
+++ src/components/EmployeeForm/form.jsx
@@ -8,20 +8,40 @@ const [employeeData, setEmployeeData] = useState('');
   return (
     <div>
       <h1>Data belongs here</h1>
-      
-  <div class="row">
-    <form class="col s8">
       <div class="row">
-        <div class="input-field col s8">
-          <input id="email" type="email" class="validate"/>
-          <label for="email">Email</label>
-          <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
-        </div>
+        <form class="col s8">
+          <div class="row">
+            <div class="input-field col s8">
+              <input id="name" type="text" class="validate" />
+              <label for="name">Last Name</label>
+              <span class="helper-text" data-error="wrong" data-success="right"/>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s8">
+              <input id="email" type="email" class="validate"/>
+              <label for="email">Email</label>
+              <span class="helper-text" data-error="wrong" data-success="right"/>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s8">
+              <input id="date" type="date" class="validate"/>
+              <label for="date">Birthdate</label>
+              <span class="helper-text" data-error="wrong" data-success="right"/>
+            </div>
+          </div>
+          {/* <input type="text" class="datepicker" />*/}
+          <div class="row">
+            <div class="input-field col s8">
+              <input id="address" type="text" class="validate" />
+              <label for="address">Address</label>
+              <span class="helper-text" data-error="wrong" data-success="right"/>
+            </div>
+          </div>
+        </form>
       </div>
-    </form>
-  </div>
-    </div>
-    
+    </div> 
   )
 }
 
